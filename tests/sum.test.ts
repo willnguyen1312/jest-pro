73be1d3af66c1e5922ac8d6fa710682806bf4228
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-12-22 09:47:49 -0400

    Oh my sum 🙈

--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -1,8 +1,14 @@
 import { describe, expect, test } from "@jest/globals";
 import { sum } from "../src/sum";
 
+import { createMock } from "ts-jest-mock";
+
+jest.mock("../src/sum");
+
 describe("sum module", () => {
   test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
+    const mockSum = createMock(sum);
+    mockSum.mockReturnValue(3);
+    expect(mockSum(1, 2)).toBe(3);
   });
 });
